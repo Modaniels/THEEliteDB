fb42ccc57174454c10ab4554e9438e59a77f19d9
AdamGachau <adamgachao06@gmail.com>
2026-05-20 21:31:26 +0300

    added user table

---
+++ Database/seeds/user_table.sql
@@ -0,0 +1,11 @@
+INSERT INTO USER (name, email, user_type) VALUES
+('James Omondi', 'jomondi@gmail.com', 'Subscriber'),
+('Sarah Wanjiku', 'swanjiku@gmail.com', 'Guest'),
+('Brian Mutua', 'bmutua@gmail.com', 'Subscriber'),
+('Grace Njoroge', 'gnjoroge@gmail.com', 'Guest'),
+('David Kipkorir', 'dkipkorir@gmail.com', 'Subscriber'),
+('Elena Akinyi', 'eakinyi@gmail.com', 'Subscriber'),
+('Peter Mwangi', 'pmwangi@gmail.com', 'Guest'),
+('Lucy Kamau', 'lkamau@gmail.com', 'Subscriber'),
+('Victor Otieno', 'votieno@gmail.com', 'Subscriber'),
+('Faith Nduta', 'fnduta@gmail.com', 'Guest');
